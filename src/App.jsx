0e5b827408c1581e3f98ce50bc58cd027fa8b427
Geraldine <giquigeraldine@outlook.com>
2023-10-13 23:32:09 +0100

    home page header complete and resposive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route} from "react-router-dom";
 import * as Pages from "./pages";
-import { NavBar} from "./components";
+// import { NavBar} from "./components";
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<NavBar />}>
+      <Route path="/">
         <Route index element={<Pages.HomePage />} />
       </Route>     
     </Routes>
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+const HomePage = () => {
+  return (
+    <>
+      <section className="header">
+        <div className="container main">
+          <div className="header--content-wraper">
+            <h1 className="header--primary"><span className="header--primary-start">FULLY STACKED</span> <br /> AND <br /> NOT GOING BACK</h1>
+            <h4 className="header--secondary">
+              <span className="header--secondary-main">GI GERALDINE HO</span>
+              <span className="header--secondary-sub"> SOFTWARE DEVELOPER</span>
+            </h4>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default HomePage
