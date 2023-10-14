aa0e3b911241a93252e2d1de34703e278eaa5b86
Geraldine <giquigeraldine@outlook.com>
2023-10-14 17:49:59 +0100

    navbar base complete

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
+
+
 const HomePage = () => {
+  // const click = () => alert('clicked')
   return (
     <>
       <header className="header">
-        {/* <div className="header__hero-wrapper"> */}
+        <div className="header__hero-wrapper">
           <div className="container header__content-flex">
             <div className="header__content-wraper">
               <h1 className="header__primary"><span className="header__primary-start">FULLY STACKED</span> <br /> AND <br /> NOT GOING BACK</h1>
@@ -12,8 +15,7 @@ const HomePage = () => {
               </h4>
             </div>
           </div>
-        {/* </div> */}
-        {/* <button>CLICK ME</button> */}
+        </div>
       </header>
     </>
   )
