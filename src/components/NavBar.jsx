cb9192a43b945b250cb323c6cfe247e68904e72e
Geraldine <giquigeraldine@outlook.com>
2023-10-14 17:16:46 +0100

    Navbar functions opening and closing on mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,22 @@
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import { useState } from 'react'
 
 const NavBar = () => {
+  const [active, setActive] = useState(false);
+
+  const click = () => setActive(!active)
+
   return (
     <>
       <nav className='navbar'>
-        <ul className='navbar--item-wraper'>
-          <li className='narbar--item'>Projects</li>
-          <li className='narbar--item'>About Me</li>
-          <li className='narbar--item'>Contact</li>
+        <button
+          onClick={click}
+          className={active ? 'navbar__button navbar__button-active' : 'navbar__button'}
+        >=</button>
+        <ul className={active ? 'navbar__list navbar__list-active' :'navbar__list' }>
+          <li className='navbar__list-item'>Projects</li>
+          <li className='navbar__list-item navbar__list-item-deco'>About Me</li>
+          <li className='navbar__list-item'>Contact</li>
         </ul>
       </nav>
       <Outlet />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,20 @@
 const HomePage = () => {
   return (
     <>
-      <section className="header">
-        <div className="container header__content-flex">
-          <div className="header__content-wraper">
-            <h1 className="header__primary"><span className="header__primary-start">FULLY STACKED</span> <br /> AND <br /> NOT GOING BACK</h1>
-            <h4 className="header__secondary">
-              <span className="header__secondary-main">GI GERALDINE HO</span>
-              <span className="header__secondary-sub"> SOFTWARE DEVELOPER</span>
-            </h4>
+      <header className="header">
+        {/* <div className="header__hero-wrapper"> */}
+          <div className="container header__content-flex">
+            <div className="header__content-wraper">
+              <h1 className="header__primary"><span className="header__primary-start">FULLY STACKED</span> <br /> AND <br /> NOT GOING BACK</h1>
+              <h4 className="header__secondary">
+                <span className="header__secondary-main">GI GERALDINE HO</span>
+                <span className="header__secondary-sub"> SOFTWARE DEVELOPER</span>
+              </h4>
+            </div>
           </div>
-        </div>
-      </section>
+        {/* </div> */}
+        {/* <button>CLICK ME</button> */}
+      </header>
     </>
   )
 }
