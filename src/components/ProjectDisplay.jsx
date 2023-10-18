383c5eddf9619075d072ae5585bf8c17ac6d84a6
Geraldine <giquigeraldine@outlook.com>
2023-10-18 12:50:44 +0100

    dangerously set html for project display content

--- src/components/ProjectDisplay.jsx
+++ src/components/ProjectDisplay.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import techList from '../helpers/techList.json';
 
-const ProjectDisplay = ({ title, role, tech, children, github, imgPreview }) => {
+const ProjectDisplay = ({ title, role, tech, content, github, imgPreview }) => {
   const [isContentVisible, setContentVisible] = useState(false);
   const [showImage, setShowImage] = useState(false);
 
   const techToRender = Array.isArray(tech)
     ? tech.map((techName, index) => techList[techName] && (
-      <div  key={index} className='project__tech-stack-img-wrapper' data-tech-name={techName}> <img src={techList[techName].imageURL} alt={`${index} image`} className='project__tech-stack-img'/> </div>
+      <div key={index} className='project__tech-stack-img-wrapper' data-tech-name={techName}> <img src={techList[techName].imageURL} alt={`${index} image`} className='project__tech-stack-img' /> </div>
     ))
     : techList[tech] && (
       <div data-tool-tip={techList[tech].name} data-tech-name={techList[tech].name} className='project__tech-stack-img-wrapper' ><img src={techList[tech].imageURL} alt={`${techList[tech].name} image`} className='project__tech-stack-img' /></div>
@@ -27,15 +27,15 @@ const ProjectDisplay = ({ title, role, tech, children, github, imgPreview }) => 
       </div>
       <div className={isContentVisible || showImage ? 'project__body animate' : 'project__body'}>
         <div className='project__content'>
-          <p className={ isContentVisible ? 'project__content-paragraph animate' :  'project__content-paragraph' }>
-            {isContentVisible ? children : `${children.slice(0, 180)}...`}
-            {children.length > 180 && (
+          <p className={isContentVisible ? 'project__content-paragraph animate' : 'project__content-paragraph'}>
+            <span dangerouslySetInnerHTML={{ __html: isContentVisible ? content : `${content.slice(0, 180)}...` }} />
+            {content.length > 180 && (
               <button onClick={toggleContentVisibility} className='project__content-btn'>
                 {isContentVisible ? 'Show Less' : 'Show More'}
               </button>
             )}
           </p>
-          <img src={imgPreview} alt="" className={ showImage ? 'project__content-image animate' :  'project__content-image' } />
+          <img src={imgPreview} alt="" className={showImage ? 'project__content-image animate' : 'project__content-image'} />
         </div>
         <div className={showImage || isContentVisible ? 'project__action-wrapper animate' : 'project__action-wrapper'}>
           <button className='project__btn view'>VIEW HERE</button>
